c96a9cddeacb4c8d0ec85fe29cedf4beb01c6667
Lenovo <Lenovo@DESKTOP-TUI37SR>
2025-04-21 15:44:33 +0530

    commit la

--- SeleniumAutomationProject/src/test/java/seleniumPractice/Selenium3.java
+++ SeleniumAutomationProject/src/test/java/seleniumPractice/Selenium3.java
@@ -12,7 +12,7 @@ public class Selenium3 {
 
 	public static void main(String[] args) throws Exception {
 
-		//add this comment just for checking
+		//remove the comment
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--incognito");
 		WebDriver driver = new ChromeDriver(options);
